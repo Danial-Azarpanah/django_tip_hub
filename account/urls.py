3bc018f4b53674bdf48f55505c8ae4995235fc58
Danial-Azarpanah <danial.azarpanah.dev@gmail.com>
2022-08-27 13:02:28 +0430

    Added forgot password (reset password with email) functionality

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -11,4 +12,23 @@ urlpatterns = [
     path("activate/(?P<uidb64>[0-9A-Za-z_\\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/",
          views.activate, name='activate'),
 
+    # password reset urls
+    path('reset-password/',
+         auth_views.PasswordResetView.as_view(template_name="account/forgot_password.html",
+                                              email_template_name="account/password_reset_email.html",
+                                              success_url=reverse_lazy('account:password_reset_done')),
+         name="reset_password"),
+
+    path('reset-password/sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name="account/password_reset_sent.html"),
+         name="password_reset_done"),
+
+    path('reset-password/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="account/password_reset_form.html",
+                                                     success_url=reverse_lazy('account:password_reset_complete')),
+         name="password_reset_confirm"),
+
+    path('reset_password/complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name="account/password_reset_done.html"),
+         name="password_reset_complete"),
 ]
--- tip_hub/settings.py
+++ tip_hub/settings.py
@@ -139,10 +139,11 @@ MEDIA_ROOT = path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'account.User'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'danial.azarpanah.dev@gmail.com'
-EMAIL_HOST_PASSWORD = 'dggyxgnvvaphnswo'
+EMAIL_HOST_USER = 'tiphub.site@gmail.com'
+EMAIL_HOST_PASSWORD = 'wizfermwxtwcgjho'
 EMAIL_PORT = 587
 
 AUTHENTICATION_BACKENDS = (
