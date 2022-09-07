c5486c31c286e8e3fa16cd97762ee303694d35a8
Danial-Azarpanah <danial.azarpanah.dev@gmail.com>
2022-09-07 14:40:31 +0430

    Show videos according to the selected category

--- video/views.py
+++ video/views.py
@@ -1,7 +1,9 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView, DetailView
+from django.views.generic.list import MultipleObjectMixin
 
-from .models import Video, Comment
+from .models import Video, Comment, Category
 
 
 class VideoListView(ListView):
@@ -25,3 +27,20 @@ def video_detail(request, pk):
         body = request.POST.get("body")
         Comment.objects.create(body=body, video=video, user=request.user, parent_id=parent_id)
     return render(request, "video/video_detail.html", context={"video": video})
+
+
+def category_detail(request, slug):
+    """
+    View for returning videos
+     of the selected category
+     """
+    category = get_object_or_404(Category, slug=slug)
+    videos = Video.objects.filter(category__title=category)
+
+    # pagination
+    page_number = request.GET.get('page')
+    paginator = Paginator(videos, 1)
+    objects_list = paginator.get_page(page_number)
+
+    context = {"videos": objects_list, "category": category}
+    return render(request, 'video/category_list.html', context)
